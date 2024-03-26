c72611407c8dbb81e7d0ae0ff219863d50b59cef
Isaac Koh <kohvern@outlook.com>
2024-03-26 18:15:22 +0800

    modifying to images
    change stall and change food so far

--- app/src/main/java/com/example/androidapp/App_Objects/Food.java
+++ app/src/main/java/com/example/androidapp/App_Objects/Food.java
@@ -1,9 +1,13 @@
 package com.example.androidapp.App_Objects;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+
+import com.example.androidapp.R;
 
 public class Food {
     private Paint platePaint;
@@ -12,14 +16,20 @@ public class Food {
 
     private double radius;
 
+    private Bitmap image;
+
     public Food(Context context, double centerScreenX, double topScreenY, double radius) {
         this.centerScreenX = centerScreenX;
-        this.topScreenY = topScreenY;
+        this.topScreenY = topScreenY + 70;
         this.radius = radius;
 
         platePaint = new Paint();
-        platePaint.setColor(Color.WHITE);
+        platePaint.setColor(Color.rgb(255, 165, 0));
         platePaint.setStyle(Paint.Style.FILL);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 2;
+        image = BitmapFactory.decodeResource(context.getResources(), R.drawable.food, options);
     }
 
     public void draw(Canvas canvas) {
@@ -29,6 +39,8 @@ public class Food {
         float plateRadius = (float) radius;
 
         // Draw plate circle
-        canvas.drawCircle(plateCenterX, plateCenterY, plateRadius, platePaint);
+        //canvas.drawCircle(plateCenterX, plateCenterY, plateRadius, platePaint);
+
+        canvas.drawBitmap(image, (float) image.getWidth() / 2, plateCenterY, null);
     }
 }
--- app/src/main/java/com/example/androidapp/App_Objects/Stall.java
+++ app/src/main/java/com/example/androidapp/App_Objects/Stall.java
@@ -1,9 +1,13 @@
 package com.example.androidapp.App_Objects;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+
+import com.example.androidapp.R;
 
 public class Stall {
     public int centerX;
@@ -11,23 +15,30 @@ public class Stall {
     public int width;
     public int height;
     private Paint paint;
+    private Bitmap image;
 
     //Initialize stall and where it is
     public Stall(Context context, int centerScreenX, int topScreenY) {
         this.centerX = centerScreenX;
         this.topY = topScreenY;
-        this.width = 200; // Set the width of the stall
-        this.height = 100; // Set the height of the stall
 
         // Initialize paint for drawing the stall
         paint = new Paint();
         paint.setColor(Color.RED); // Set the color of the stall to red
         paint.setStyle(Paint.Style.FILL); // Set the style to fill
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 8;
+        image = BitmapFactory.decodeResource(context.getResources(), R.drawable.table, options);
+
+        this.width = image.getWidth(); // Set the width of the stall
+        this.height = image.getHeight(); // Set the height of the stall
     }
 
     //Drawing my stall
     public void draw(Canvas canvas) {
         // Draw the stall as a rectangle
-        canvas.drawRect(centerX - (float) width / 2, topY, centerX + (float) width / 2, topY + height, paint);
+        //canvas.drawRect(centerX - (float) width / 2, topY, centerX + (float) width / 2, topY + height, paint);
+        canvas.drawBitmap(image, centerX - (float) image.getWidth() / 2, topY, null);
     }
 }
