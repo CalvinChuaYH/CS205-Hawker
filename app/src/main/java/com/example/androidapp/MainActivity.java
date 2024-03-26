54bd57378aa9e9b577c0a180ac307f7252f02be8
CalvinChuaYH <calvinchuayh@gmail.com>
2024-03-26 19:28:41 +0800

    Added game prompt before starting game

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -26,10 +28,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startGame(View view)  {
-        startActivity(new Intent(this, GameActivity.class));
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Hawker Heroes");
+        builder.setMessage("You are Mr Tan, a 40 year old hawker operating a carrot cake store in Singapore. " +
+                "The year is 2100, robots are wandering amongst us. Surprisingly, their favourite food is carrot cake." +
+                "Your mission is to serve them as fast as possible!");
+        builder.setPositiveButton("Let's Start Serving!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Handle the positive button click
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 
     public void viewLeaderboard(View view) {
         startActivity(new Intent(this, LeaderboardActivity.class));
+    }
+
+    public void showPrompt() {
+
+
+
     }
 }
